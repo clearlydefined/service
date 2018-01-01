3cc790c1ea2be97fbced08bc2e998fce74e2ae27
Jeff McAffer <jmcaffer@microsoft.com>
2018-01-01 14:45:05 -0800

    add  a crawler-based harvest provider

---
+++ providers/harvest/crawler.js
@@ -0,0 +1,26 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+const requestPromise = require('request-promise-native');
+
+class CrawlingHarvester {
+
+  constructor(options) {
+    this.options = options;
+  }
+
+  async harvest(spec) {
+    const headers = {
+      Authorization: 'basic' + this.options.authToken
+    };
+    return requestPromise({
+      url: `${this.options.url}/requests`,
+      method: 'POST',
+      body: spec,
+      headers,
+      json: true
+    });
+  }
+}
+
+module.exports = options => new CrawlingHarvester(options);
