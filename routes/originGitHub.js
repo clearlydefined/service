88243c6508e03ee6b1b11b98406dac843b047d2c
Jeff McAffer <jeff.mcaffer@microsoft.com>
2018-01-20 21:50:16 -0800

    fix lint issues

--- routes/originGitHub.js
+++ routes/originGitHub.js
@@ -19,7 +19,7 @@ router.get('/:login/:repo/revisions', asyncMiddleware(async (request, response) 
     const github = getClient(request);
     const answer = await github.gitdata.getTags({ owner: login, repo, per_page: 100 });
     const result = answer.data
-      .map(item => { return { tag: item.ref.slice(10), sha: item.object.sha } })
+      .map(item => { return { tag: item.ref.slice(10), sha: item.object.sha }; })
       .sort((a, b) => a.tag < b.tag ? 1 : (a.tag > b.tag) ? -1 : 0);
     return response.status(200).send(result);
   } catch (error) {
@@ -35,11 +35,11 @@ router.get('/:login/:repo?', asyncMiddleware(async (request, response) => {
   const github = getClient(request);
   if (request.path.indexOf('/', 1) > 0) {
     const answer = await github.search.repos({ q: `${repo || ''}+user:${login}+in:name+fork:true`, sort: 'stars', per_page: 100 });
-    const result = answer.data.items.map(item => { return { id: item.full_name } });
+    const result = answer.data.items.map(item => { return { id: item.full_name }; });
     return response.status(200).send(result);
   }
-  const answer = await github.search.users({ q: `${login}+repos:>0`, sort: 'repositories', per_page: 30 })
-  const result = answer.data.items.map(item => { return { id: item.login } });
+  const answer = await github.search.users({ q: `${login}+repos:>0`, sort: 'repositories', per_page: 30 });
+  const result = answer.data.items.map(item => { return { id: item.login }; });
   return response.status(200).send(result);
 }));
 
