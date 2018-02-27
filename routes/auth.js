242cf3537368582d441c689c54bf0f8da70a006e
Jacob Peddicord <peddicor@amazon.com>
2018-02-27 11:32:00 -0800

    Allow localhost to access dev-api
    This is accomplished by checking if the referrer is localhost, and then
    setting a cookie with the real localhost origin. This cookie is read on
    finalize and substituted for the real UI in postMessage.

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const passport = require('passport');
 const GitHubStrategy = require('passport-github').Strategy;
+const { URL } = require('url');
 
 const config = require('../lib/config');
 
@@ -24,17 +25,46 @@ function passportOrPat() {
   };
 }
 
-router.get('/github', passportOrPat(), (req, res) => {
+router.get('/github', (req, res) => {
+  // a shim to allow for a localhost UI to point to dev-api.
+  // without this, /github/finalize won't be able to postMessage due to
+  // cross-origin constraints. we're checking if the referrer (UI) is on
+  // localhost, and if so, store the full origin (incl. port) in a cookie
+  // to be read during /github/finalize.
+  const referrer = req.get('Referrer');
+  if (referrer) {
+    try {
+      const url = new URL(referrer);
+
+      if (url.hostname === 'localhost') {
+        res.cookie('localhostOrigin', url.origin);
+      }
+    } catch (err) {
+      console.warn('Referrer parsing error, ignoring', err);
+    }
+  }
+
+  res.redirect('github/start');
+});
+
+router.get('/github/start', passportOrPat(), (req, res) => {
   // this only runs if passport didn't kick in above, but double
   // check for sanity in case upstream changes
   if (!config.auth.github.clientId) {
-    res.redirect('github/finalize');
+    res.redirect('finalize');
   }
 });
 
 router.get('/github/finalize', passportOrPat(),
   (req, res) => {
     const safeToken = encodeURIComponent(req.user.githubAccessToken);
+    let origin = config.endpoints.website;
+
+    // allow for sending auth responses to localhost on dev site; see /github
+    // route above. real origin is stored in cookie.
+    if (config.endpoints.service.includes('dev-api') && req.cookies.localhostOrigin) {
+      origin = req.cookies.localhostOrigin;
+    }
 
     // passing in the 'website' endpoint below is very important;
     // using '*' instead means this page will gladly send out a
@@ -44,7 +74,7 @@ router.get('/github/finalize', passportOrPat(),
       window.opener.postMessage({
         type: 'github-token',
         token: '${safeToken}',
-      }, '${config.endpoints.website}');
+      }, '${origin}');
       window.close();
     </script>`);
   }
