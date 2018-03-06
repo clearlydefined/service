1aff017aa331c00c25091dd278abf345a65f380a
Gene Hazan <ghazan@microsoft.com>
2018-03-06 13:58:53 -0800

    Code review

--- business/cacheRefresher.js
+++ business/cacheRefresher.js
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation and others. Licensed under the MIT license.
 // SPDX-License-Identifier: MIT
 
+const appInsights = require('applicationinsights')
 const DefinitionsCache = require('./definitionsCache')
 const utils = require('../lib/utils')
 const _ = require('lodash')
@@ -9,7 +10,18 @@ class CacheRefresher {
   constructor(harvest, curation) {
     this.harvestService = harvest
     this.curationService = curation
-    setInterval(this.refreshDefinitions.bind(this), 4 * 60 * 1000)
+    setInterval(this.refresh.bind(this), 5 * 60 * 1000)
+  }
+
+  async refresh() {
+    try {
+      await this.refreshDefinitions()
+    } catch (error) {
+      appInsights.defaultClient.trackException({
+        exception: error,
+        properties: { eventName: 'CacheRefreshError' }
+      })
+    }
   }
 
   async refreshDefinitions() {
@@ -18,7 +30,7 @@ class CacheRefresher {
     const harvest = await this.harvestService.list(coordinates)
     const stringHarvest = harvest.map(c => c.toString())
     const result = _.union(stringHarvest, curated)
-    DefinitionsCache.set(coordinates, result, 5 * 60 * 1000)
+    DefinitionsCache.set(coordinates, result, 10 * 60 * 1000)
   }
 }
 
