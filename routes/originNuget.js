d2f7d52ed81c9921b4e92b50db71f90a3f14199a
Teju Manchenella <tejumanchenella@Tejus-MBP-2.guest.corp.microsoft.com>
2018-07-13 16:38:17 -0700

    removed namespace and returned new id field value instead of title

--- routes/originNuget.js
+++ routes/originNuget.js
@@ -6,27 +6,25 @@ const router = require('express').Router()
 const requestPromise = require('request-promise-native')
 
 router.get(
-  '/:namespace?/:name/revisions',
+  '/:name/revisions',
   asyncMiddleware(async (request, response) => {
     const baseUrl = 'https://api-v2v3search-0.nuget.org'
-    const { namespace, name } = request.params
-    const fullName = namespace ? `${namespace}/${name}` : name
-    const url = `${baseUrl}/autocomplete?id=${fullName}&prerelease=true`
+    const { name } = request.params
+    const url = `${baseUrl}/autocomplete?id=${name}&prerelease=true`
     const answer = await requestPromise({ url, method: 'GET', json: true })
     return response.status(200).send(answer.data)
   })
 )
 
 router.get(
-  '/:namespace/:name?',
+  '/:name',
   asyncMiddleware(async (request, response) => {
     const baseUrl = 'https://api-v2v3search-0.nuget.org'
-    const { namespace, name } = request.params
-    const fullName = name ? `${namespace}/${name}` : namespace
-    const url = `${baseUrl}/query?q=${fullName}`
+    const { name } = request.params
+    const url = `${baseUrl}/query?q=${name}`
     const answer = await requestPromise({ url, method: 'GET', json: true })
     const result = answer.data.map(entry => {
-      return { id: entry.title }
+      return { id: entry.id }
     })
     return response.status(200).send(result)
   })
