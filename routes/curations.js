f5bb95289462abfd62da102bf9f597d4dcff27cc
Teju Manchenella <tejumanchenella@Tejus-MBP-2.guest.corp.microsoft.com>
2018-07-16 15:11:29 -0700

    clean up code

--- routes/curations.js
+++ routes/curations.js
@@ -6,7 +6,7 @@ const express = require('express')
 const router = express.Router()
 const utils = require('../lib/utils')
 const Curation = require('../lib/curation')
-const { some } = require('lodash')
+
 // Get a proposed patch for a specific revision of a component
 router.get(
   '/:type/:provider/:namespace/:name/:revision/pr/:pr',
