fc6aba7fbc61b3e316865db42f85782c3453e6c8
Daniel Landau <daniel@landau.fi>
2018-09-26 11:14:01 +0300

    Get commit hash instead of annotated tag hash

--- routes/originGitHub.js
+++ routes/originGitHub.js
@@ -20,13 +20,30 @@ router.get(
       const { login, repo } = request.params
       const github = getClient(request)
       const answer = await github.gitdata.getTags({ owner: login, repo, per_page: 100 })
-      const result = answer.data
-        .map(item => {
-          return { tag: item.ref.slice(10), sha: item.object.sha }
-        })
+      const unsorted = await Promise.all(answer.data
+        .map(async item => {
+          if (item.object.type === 'commit')
+            return { tag: item.ref.slice(10), sha: item.object.sha }
+
+          if (item.object.type !== 'tag') {
+            return null
+          }
+
+          // we know now that we have an annotated tag, get the associated commit hash
+          let response
+          try {
+            response = await github.gitdata.getTag({ owner: login, repo, sha: item.object.sha })
+          } catch (e) {
+            console.error(e)
+            return null
+          }
+          return { tag: item.ref.slice(10), sha: response.data.object.sha }
+        }))
+      const result = unsorted.filter(x => x)
         .sort((a, b) => (a.tag < b.tag ? 1 : a.tag > b.tag ? -1 : 0))
       return response.status(200).send(result)
     } catch (error) {
+      console.error(error)
       if (error.code === 404) return response.status(200).send([])
       // TODO what to do on non-404 errors? Log for sure but what do we give back to the caller?
       return response.status(200).send([])
