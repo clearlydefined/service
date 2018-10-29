71a00dc5d9d5342c0f443f529739c0e90396a9ec
moranthomas <tom_moran@hotmail.com>
2018-10-29 10:09:43 +0000

    adding PR feedback for 205 into azureSearch request promise

--- providers/search/azureSearch.js
+++ providers/search/azureSearch.js
@@ -39,24 +39,38 @@ class AzureSearch extends AbstractSearch {
    * Index the given definition in the search system
    * @param {Definition or Definition[]} definitions - the definition(s) to index
    */
-  store(definitions) {
-    const entries = this._getEntries(Array.isArray(definitions) ? definitions : [definitions])
-    return requestPromise({
-      method: 'POST',
-      url: this._buildUrl(`indexes/${coordinatesIndexName}/docs/index`),
-      headers: this._getHeaders(),
-      body: { value: entries },
-      withCredentials: false,
-      resolveWithFullResponse: true
-    })
-    .then(function (res) {
-        console.log("POST returned with status %d", handleResponseCodes(res.statusCode));
-    })
-    .catch(function (err) {
-        console.log("POST failed with status %d", err.statusCode);
-        console.log(err)
-    });
-  }
+
+   store(definitions) {
+     const entries = this._getEntries(Array.isArray(definitions) ? definitions : [definitions])
+
+     const cases = {
+       200: '200 OK -  Success',
+       207: '207 - At least one item was not successfully indexed',
+       400: '400 - Bad Request',
+       401: '401 - Unauthorized',
+       404: '404 - Not Found',
+       429: '429 - You have exceeded your quota on the number of documents per index.',
+       503: '503 -The system is under heavy load and your request cannot be processed at this time.'
+     }
+     const options = {
+       method: 'POST',
+       url: this._buildUrl(`indexes/${coordinatesIndexName}/docs/index`),
+       headers: this._getHeaders(),
+       body: { value: entries },
+       withCredentials: false,
+       resolveWithFullResponse: true
+     }
+
+     try{
+         const result = await requestPromise(options, function(err, res, body) {
+           if(cases[res.statusCode] === '200') {
+             return cases[res.statusCode]
+           }
+         })
+     } catch (err) {
+         return cases[err.statusCode]
+     }
+   }
 
   _getEntries(definitions) {
     return definitions.map(definition => {
@@ -80,23 +94,37 @@ class AzureSearch extends AbstractSearch {
    * Deletely the identified definition from the search system
    * @param {EntityCoordinates} coordinates - the coordinates of the definition to delete
    */
-  delete(coordinates) {
-    return requestPromise({
-      method: 'POST',
-      url: this._buildUrl(`indexes/${coordinatesIndexName}/docs/index`),
-      headers: this._getHeaders(),
-      body: { value: [{ '@search.action': 'delete', key: this._getKey(coordinates) }] },
-      withCredentials: false,
-      resolveWithFullResponse: true
-    })
-    .then(function (res) {
-        console.log("POST returned with status %d", handleResponseCodes(res.statusCode));
-    })
-    .catch(function (err) {
-        console.log("POST failed with status %d", err.statusCode);
-        console.log(err)
-    });
-  }
+   delete(coordinates) {
+
+     const cases = {
+       200: '200 OK -  Success',
+       207: '207 - At least one item was not successfully indexed',
+       400: '400 - Bad Request',
+       401: '401 - Unauthorized',
+       404: '404 - Not Found',
+       429: '429 - You have exceeded your quota on the number of documents per index.',
+       503: '503 -The system is under heavy load and your request cannot be processed at this time.'
+     }
+     const options = {
+       method: 'POST',
+       url: this._buildUrl(`indexes/${coordinatesIndexName}/docs/index`),
+       headers: this._getHeaders(),
+       body: { value: [{ '@search.action': 'delete', key: this._getKey(coordinates) }] },
+       withCredentials: false,
+       resolveWithFullResponse: true
+     }
+
+     try{
+         const result = await requestPromise(options, function(err, res, body) {
+           if(cases[res.statusCode] === '200') {
+             return cases[res.statusCode]
+           }
+         })
+     } catch (err) {
+         return cases[err.statusCode]
+     }
+   }
+
 
   _getKey(coordinates) {
     return base64.encode(coordinates.toString())
@@ -136,39 +164,34 @@ class AzureSearch extends AbstractSearch {
   }
 
   _createIndex(body) {
-    return requestPromise({
+
+    const cases = {
+      200: '200 OK -  Success',
+      207: '207 - At least one item was not successfully indexed',
+      400: '400 - Bad Request',
+      401: '401 - Unauthorized',
+      404: '404 - Not Found',
+      429: '429 - You have exceeded your quota on the number of documents per index.',
+      503: '503 -The system is under heavy load and your request cannot be processed at this time.'
+    }
+    const options = {
       method: 'POST',
       url: this._buildUrl('indexes'),
       headers: this._getHeaders(),
       body,
       withCredentials: false,
       resolveWithFullResponse: true
-    })
-    .then(function (res) {
-        console.log("POST returned with status %d", handleResponseCodes(res.statusCode));
-    })
-    .catch(function (err) {
-        console.log("POST failed with status %d", err.statusCode);
-        console.log(err)
-    });
-  }
+    }
 
-  function handleResponseCodes(statusCode){
-      switch (statusCode) {
-      case 200:
-          text = "200 OK - Success";
-          break;
-      case 207:
-          text = "207 - At least one item was not successfully indexed";
-          break;
-      case 429:
-          text = "429 - You have exceeded your quota on the number of documents per index.";
-          break;
-      case 503:
-          text = "503 -The system is under heavy load and your request can't be processed at this time.";
-          break;
-      }
-      return text;
+    try{
+        const result = await requestPromise(options, function(err, res, body) {
+          if(cases[res.statusCode] === '200') {
+            return cases[res.statusCode]
+          }
+        })
+    } catch (err) {
+        return cases[err.statusCode]
+    }
   }
 
   async _hasIndex(name) {
