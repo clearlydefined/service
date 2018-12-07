8e3e6e6858ccb09f9e3015ff1c292bedeb7b0327
moranthomas <thomas@optimizme.com>
2018-12-07 15:11:59 +0000

    adding sourceLocation suggestions

--- business/suggestionService.js
+++ business/suggestionService.js
@@ -21,33 +21,36 @@ class SuggestionService {
     const related = await this._getRelatedDefinitions(coordinates)
     if (!related) return null
     const baseSuggestion = this._createBaseSuggestion(coordinates)
-    // probably create a struct with the strings in it and iterate instead
+    // create a struct with the strings in it and iterate instead
     const promises = [
       await this._collectSuggestionsForField(related, baseSuggestion, 'licensed.declared'),
+      await this._collectSuggestionsForField(related, baseSuggestion, 'described.sourceLocation'),
       await this._collectSuggestionsForField(related, baseSuggestion, 'described.releaseDate')
     ]
     const result = await Promise.all(promises)
     return result
   }
 
-  // All the related contents correspond to other definitions in harvested-data-definition
-  // The only differentiator for now is the revision and using mock test data
+  /**
+   * Get related definitions for the given coordinates.
+   * Related here means other definitions for the same component with different revisions
+   *
+   * @param {EntityCoordinates} coordinates - The entity we are looking for related defintions to
+   */
   async _getRelatedDefinitions(coordinates) {
     const related = await this.definitionStore.list(coordinates.asRevisionless(), 'definitions')
-    // If the related array only has one entry then return early (lodash isEmpty won't work)
+    // If the related array only has one entry then return early
     if (Object.keys(related).length <= 1) {
       return
     }
-    // Create a coordinatesList from the array of strings
     const coordinatesList = []
     related.forEach(element => {
       coordinatesList.push(EntityCoordinates.fromString(element))
     })
-    // Get all the definition entries available for the given coordinates (down to the revision)
     const validDefinitions = await this.definitionService.getAll(coordinatesList)
     const sortedByReleaseDate = sortBy(validDefinitions, ['described.releaseDate'])
 
-    // Now just split the definitions to those before supplied coords and those after
+    // Split the definitions into before supplied coords and those after
     const index = sortedByReleaseDate.findIndex(entry => entry.coordinates.revision === coordinates.revision)
     if (index === -1) return null
     const before = sortedByReleaseDate.slice(Math.max(index - 3, 0), index).reverse()
@@ -61,52 +64,26 @@ class SuggestionService {
     }
   }
 
+  /**
+   * Collect Suggestions for a given related definition and field
+   * Only give suggestions if there is something missing
+   */
   _collectSuggestionsForField(related, suggestions, field) {
-    //_collectLicenseSuggestions(related, suggestions) {
-    // for now only do suggestions if there is something missing
     const definition = related.sortedByReleaseDate[related.index]
     // If there is already a declared licence or there are no related entries then return early
-    if (get(definition, field) || !(related.before.length + related.after.length)) return
-
+    if (get(definition, field) || !(related.before.length + related.after.length)) return []
     const before = filter(related.before, entry => get(entry, field))
     const after = filter(related.after, entry => get(entry, field))
-
-    // Now create a new merged array from both the before and after arrays
+    // Merged the before and after arrays
     const suggestionDefinitions = concat(before, after)
-    const splitFields = field.split('.')
-    const fieldOne = splitFields[0]
-    const fieldTwo = splitFields[1]
     set(
       suggestions,
       field,
       suggestionDefinitions.map(suggestion => {
-        return { value: suggestion[fieldOne][fieldTwo] }
+        return { value: get(suggestion, field) }
       })
     )
-  }
-
-  /********** ORIG *******/
-  _collectSourceLocationSuggestions(related, suggestions) {
-    // for now only do suggestions if there is something missing
-    const definition = related.sortedByReleaseDate[related.index]
-    // If there is already a declared licence or there are no related entries then return early
-    if (get(definition, 'licensed.declared') || !(related.before.length + related.after.length)) return
-
-    const before = filter(related.before, entry => get(entry, 'licensed.declared'))
-    const after = filter(related.after, entry => get(entry, 'licensed.declared'))
-
-    // Now create a new merged array from both the before and after arrays
-    const suggestionDefinitions = concat(before, after)
-
-    //{ "revision": "0.0.2", "value": "Apache-2.0" ,"date": '2018-11-19', "curation": '365', "curator": '@storrisi'  }
-    set(suggestions, 'licensed.declared.value', suggestionDefinitions.map(suggestion => suggestion.licensed.declared))
-    set(suggestions, 'described.releaseDate', suggestionDefinitions.map(suggestion => suggestion.described.releaseDate))
-    set(
-      suggestions,
-      'described.sourceLocation',
-      suggestionDefinitions.map(suggestion => suggestion.described.sourceLocation)
-    )
-    set(suggestions, 'revision', suggestionDefinitions.map(suggestion => suggestion.coordinates.revision))
+    return suggestions
   }
 }
 
