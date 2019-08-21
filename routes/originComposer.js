08f1623a38ff9cd99683a2f7fcc19bbe12538f50
Fredrick Kofi Tam <Fredrick-Tam@github.com>
2019-08-20 17:29:44 -0700

    add composer to routes, definitions and curations

--- routes/originComposer.js
+++ routes/originComposer.js
@@ -1,2 +1,35 @@
 // Copyright (c) Microsoft Corporation and others. Licensed under the MIT license.
 // SPDX-License-Identifier: MIT
+
+const asyncMiddleware = require('../middleware/asyncMiddleware')
+const router = require('express').Router()
+const requestPromise = require('request-promise-native')
+const { uniq } = require('lodash')
+
+router.get(
+  '/:namespace?/:name/revisions',
+  asyncMiddleware(async (request, response) => {
+    const { name } = request.params
+    const url = `https://packagist.org/packages/${name}.json`
+    const answer = await requestPromise({ url, method: 'GET', json: true })
+    const result = answer.package.versions
+    return response.status(200).send(result)
+  })
+)
+
+router.get(
+  '/:name',
+  asyncMiddleware(async (request, response) => {
+    const { name } = request.params
+    const url = `https://packagist.org/packages/${name}.json`
+    const answer = await requestPromise({ url, method: 'GET', json: true })
+    const result = answer.package
+    return response.status(200).send(result)
+  })
+)
+
+function setup() {
+  return router
+}
+
+module.exports = setup
