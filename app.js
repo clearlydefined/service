d5782cdd5ef660ec4e2cca3c825023ee7f20da7a
Nell Shamrell <nells@microsoft.com>
2021-01-15 13:31:43 -0800

    restore these files to what is in master
    Signed-off-by: Nell Shamrell <nells@microsoft.com>

--- app.js
+++ app.js
@@ -163,8 +163,7 @@ function createApp(config) {
     Promise.all(initializers.map(init => init())).then(
       async () => {
         // Bit of trick for local hosting. Preload search if using an in-memory search service
-        // Commenting out because I believe this is broken
-        // if (searchService.constructor.name === 'MemorySearch') await definitionService.reload('definitions')
+        if (searchService.constructor.name === 'MemorySearch') await definitionService.reload('definitions')
         logger.info('Service initialized', { buildNumber: process.env.BUILD_NUMBER })
 
         // kick off the queue processors
