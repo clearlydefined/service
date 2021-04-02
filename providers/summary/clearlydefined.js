e20e1396b9127ece76f5eebe69063c44eac5c2b1
pnibakuze-ms <81187486+pnibakuze-ms@users.noreply.github.com>
2021-04-02 11:43:40 -0400

    Added initial changes for Google Maven Support

--- providers/summary/clearlydefined.js
+++ providers/summary/clearlydefined.js
@@ -133,20 +133,41 @@ class ClearlyDescribedSummarizer {
     setIfValue(result, 'licensed.declared', uniq(licenses).join(' AND '))
   }
 
-  addMavenData(result, data, coordinates) {
+  getMavenUrls(coordinates) {
+    var urls = { download: "", registry: "" }
     const namespaceAsFolders = coordinates.namespace ? coordinates.namespace.replace(/\./g, '/') : coordinates.namespace
+
+    switch (coordinates.provider) {
+      case 'mavencentral':
+        urls.registry = `https://repo1.maven.org/maven2/${namespaceAsFolders}/${coordinates.name}`
+        urls.download = `https://repo1.maven.org/maven2/${namespaceAsFolders}/${coordinates.name}/${coordinates.revision}/${coordinates.name}-${coordinates.revision}.jar`
+        break
+
+      case 'mavengoogle':
+        urls.registry = `https://maven.google.com/web/index.html#${coordinates.namespace}:${coordinates.name}`
+        urls.download = `https://dl.google.com/android/maven2/${namespaceAsFolders}/${coordinates.name}/${coordinates.revision}/${coordinates.name}-${coordinates.revision}.aar`
+        break
+
+      default:
+    }
+
+    return urls
+  }
+
+  addMavenData(result, data, coordinates) {
+    const urls = this.getMavenUrls(coordinates)
 
     setIfValue(result, 'described.releaseDate', extractDate(data.releaseDate))
     setIfValue(
       result,
       'described.urls.registry',
-      `https://repo1.maven.org/maven2/${namespaceAsFolders}/${coordinates.name}`
+      urls.registry
     )
     setIfValue(result, 'described.urls.version', `${get(result, 'described.urls.registry')}/${coordinates.revision}`)
     setIfValue(
       result,
       'described.urls.download',
-      `https://repo1.maven.org/maven2/${namespaceAsFolders}/${coordinates.name}/${coordinates.revision}/${coordinates.name}-${coordinates.revision}.jar`
+      urls.download
     )
     const projectSummaryLicenses =
       get(data, 'manifest.summary.licenses') || get(data, 'manifest.summary.project.licenses') // the project layer was removed in 1.2.0
@@ -177,18 +198,20 @@ class ClearlyDescribedSummarizer {
   }
 
   addSourceArchiveData(result, data, coordinates) {
+    const urls = this.getMavenUrls(coordinates)
+
     setIfValue(result, 'described.releaseDate', extractDate(data.releaseDate))
     const namespaceAsFolders = coordinates.namespace ? coordinates.namespace.replace(/\./g, '/') : coordinates.namespace
     setIfValue(
       result,
       'described.urls.registry',
-      `https://repo1.maven.org/maven2/${namespaceAsFolders}/${coordinates.name}`
+      urls.registry
     )
     setIfValue(result, 'described.urls.version', `${get(result, 'described.urls.registry')}/${coordinates.revision}`)
     setIfValue(
       result,
       'described.urls.download',
-      `https://repo1.maven.org/maven2/${namespaceAsFolders}/${coordinates.name}/${coordinates.revision}/${coordinates.name}-${coordinates.revision}.jar`
+      urls.download
     )
   }
 
---
+++ routes/originMavenGoogle.js
@@ -0,0 +1,34 @@
+// Copyright (c) Microsoft Corporation and others. Licensed under the MIT license.
+// SPDX-License-Identifier: MIT
+
+const asyncMiddleware = require('../middleware/asyncMiddleware')
+const router = require('express').Router()
+const requestPromise = require('request-promise-native')
+const parseString = promisify(require('xml2js').parseString)
+const { uniq } = require('lodash')
+
+// Get versions
+router.get(
+  '/:group/:artifact/revisions',
+  asyncMiddleware(async (request, response) => {
+    try {
+      const { group, artifact } = request.params
+      const groupFullPath = `${group.replace(/\./g, '/')}`
+      const url = `https://dl.google.com/android/maven2/"${groupFullPath}"/"${artifact}"/group-index.xml`
+      const answerXml = await requestPromise({ url, method: 'GET', json: false })
+      const answer = await parseString(answerXml)
+      const result = answer.response.docs.map(item => item.v)
+      return response.status(200).send(uniq(result))
+    } catch (error) {
+      if (error.code === 404) return response.status(200).send([])
+      // TODO what to do on non-404 errors? Log for sure but what do we give back to the caller?
+      return response.status(200).send([])
+    }
+  })
+)
+
+function setup() {
+  return router
+}
+
+module.exports = setup
