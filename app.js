8c057670781451cfd7ca22337cecacae2124ac85
Nell Shamrell <nells@microsoft.com>
2021-10-28 16:17:40 -0700

    adds ability to get go package revisions through the service API
    Signed-off-by: Nell Shamrell <nells@microsoft.com>

--- app.js
+++ app.js
@@ -142,6 +142,7 @@ function createApp(config) {
   app.use('/origins/rubygems', require('./routes/originRubyGems')())
   app.use('/origins/deb', require('./routes/originDeb')())
   app.use('/origins/gitlab', require('./routes/originGitLab')())
+  app.use('/origins/go', require('./routes/originGo')())
   app.use('/harvest', harvestRoute)
   app.use(bodyParser.json())
   app.use('/curations', curationsRoute)
