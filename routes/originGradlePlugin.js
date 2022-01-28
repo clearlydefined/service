12ef575f9ea7e0906d6c293c8b7e812a62a06d60
Qing Tomlinson <qing.tomlinson@sap.com>
2022-01-27 18:37:10 -0800

    Added mapping gradle coordinates without revision

--- routes/originGradlePlugin.js
+++ routes/originGradlePlugin.js
@@ -3,20 +3,20 @@
 
 const asyncMiddleware = require('../middleware/asyncMiddleware')
 const router = require('express').Router()
-const requestPromise = require('request-promise-native')
 const { get, uniq } = require('lodash')
 const { promisify } = require('util')
 const parseXml = promisify(require('xml2js').parseString)
 const GradleCoordinatesMapper = require('../lib/gradleCoordinatesMapper')
 
 // Gradle plugin documentation: https://docs.gradle.org/current/userguide/plugins.html
+const gradleHelper = new GradleCoordinatesMapper()
 
 // Get versions
 router.get(
   '/:pluginId/revisions',
   asyncMiddleware(async (request, response) => {
     const { pluginId } = request.params
-    const answer = await getMavenMetadata(pluginId)
+    const answer = await gradleHelper.getMavenMetadata(pluginId)
     const meta = answer && await parseXml(answer)
     const result = get(meta, 'metadata.versioning.0.versions.0.version') || []
     result.reverse()
@@ -29,21 +29,11 @@ router.get(
   '/:pluginId',
   asyncMiddleware(async (request, response) => {
     const { pluginId } = request.params
-    const isValid = await getMavenMetadata(pluginId)
+    const isValid = await gradleHelper.getMavenMetadata(pluginId)
     const result = isValid ? [{ id: pluginId }] : []
     return response.status(200).send(result)
   })
 )
-
-async function getMavenMetadata(name) {
-  const plugInfo = GradleCoordinatesMapper.buildPluginInfo({ name })
-  const url = `${plugInfo.pluginBaseUrl}/maven-metadata.xml`
-  return requestPromise({ url, method: 'GET', json: false })
-    .catch(error => {
-      if (error.statusCode === 404) return null
-      throw error
-    })
-}
 
 function setup() {
   return router
--- test/lib/gradleCoordinatesMapper.js
+++ test/lib/gradleCoordinatesMapper.js
@@ -16,14 +16,14 @@ describe('GradleCoordinatesMapper', () => {
   })
 
   it('spec with namespace', async () => {
-    sinon.stub(coordinatesMapper, '_query').rejects('should not be called')
+    sinon.stub(coordinatesMapper, '_handleRequest').rejects('should not be called')
     const specWithNameSpace = 'maven/gradleplugin/namespace/org.springframework.boot/1.4.2.RELEASE'
     const mapped = await coordinatesMapper.map(EntityCoordinates.fromString(specWithNameSpace))
     expect(mapped).to.be.null
   })
 
   it('spec without namespace', async () => {
-    const stub = sinon.stub(coordinatesMapper, '_query').resolves(fs.readFileSync('test/fixtures/maven/pom.xml'))
+    const stub = sinon.stub(coordinatesMapper, '_handleRequest').resolves(fs.readFileSync('test/fixtures/maven/pom.xml'))
     const specWithoutNameSpace = 'maven/gradleplugin/-/org.springframework.boot/1.4.2.RELEASE'
     const mapped = await coordinatesMapper.map(EntityCoordinates.fromString(specWithoutNameSpace))
 
@@ -34,8 +34,35 @@ describe('GradleCoordinatesMapper', () => {
   })
 
   it('invalid spec handle 404', async () => {
-    sinon.stub(coordinatesMapper, '_query').rejects({ statusCode: 404 })
+    sinon.stub(coordinatesMapper, '_handleRequest').rejects({ statusCode: 404 })
     const mapped = await coordinatesMapper.map(EntityCoordinates.fromString('maven/gradleplugin/-/name/invalid'))
     expect(mapped).not.to.be.ok
+  })
+
+  it('spec without revision', async () => {
+    sinon.stub(coordinatesMapper, '_handleRequest')
+      .withArgs('https://plugins.gradle.org/m2/pluginId/pluginId.gradle.plugin/maven-metadata.xml')
+      .resolves(fs.readFileSync('test/fixtures/maven/maven-metadata.xml'))
+      .withArgs('https://plugins.gradle.org/m2/pluginId/pluginId.gradle.plugin/4.5.10/pluginId.gradle.plugin-4.5.10.pom')
+      .resolves(fs.readFileSync('test/fixtures/maven/pom.xml'))
+    const mapped = await coordinatesMapper.map(EntityCoordinates.fromString('maven/gradleplugin/-/pluginId'))
+    expect(mapped.revision).not.to.be.ok
+    expect(mapped.namespace).to.be.equal('org.springframework.boot')
+    expect(mapped.name).to.be.equal('spring-boot-gradle-plugin')
+  })
+
+  it('_getLatestVersion', async () => {
+    const stub = sinon.stub(coordinatesMapper, '_handleRequest').resolves(fs.readFileSync('test/fixtures/maven/maven-metadata.xml'))
+    const latest = await coordinatesMapper._getLatestVersion({ name: 'pluginId' })
+    stub.calledOn('https://plugins.gradle.org/m2/pluginId/pluginId.gradle.plugin/maven-metadata.xml')
+    expect(latest).to.be.equal('4.5.10')
+  })
+
+  it('getMavenMetadata', async () => {
+    const metadata = fs.readFileSync('test/fixtures/maven/maven-metadata.xml')
+    const stub = sinon.stub(coordinatesMapper, '_handleRequest').resolves(metadata)
+    const retrieved = await coordinatesMapper.getMavenMetadata('name')
+    stub.calledOn('https://plugins.gradle.org/m2/pluginId/pluginId.gradle.plugin/maven-metadata.xml')
+    expect(retrieved).to.be.equal(metadata)
   })
 })
