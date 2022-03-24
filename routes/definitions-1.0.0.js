122916be76f462a1e3d7909e4dcf1bf7def29416
Michael Zeng <miczeng@microsoft.com>
2022-03-24 15:52:13 -0700

    force has been transformed to boolean

--- routes/definitions-1.0.0.js
+++ routes/definitions-1.0.0.js
@@ -14,7 +14,7 @@ async function getDefinition(req, resp) {
   const {
     coordinates, pr, expand
   } = req.query
-  const force = req.query.force === 'true'
+  const force = req.query.force
   let coordinatesEntity = EntityCoordinates.fromString(coordinates)
   const isValid = validator.validate('definitions-get-dto', {
     coordinates: coordinatesEntity || undefined,
