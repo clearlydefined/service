2c7f8242f8c7c613bbd8037e178cb5684ac52016
harrider <22509508+harrider@users.noreply.github.com>
2024-11-20 11:25:20 -0500

    Control heap stats logging with env vars

--- app.js
+++ app.js
@@ -101,53 +101,66 @@ function createApp(config) {
   )
 
   // ===================================================
-  // Log the heap space statistics at regular intervals
+  // Log the heap statistics at regular intervals
   // ===================================================
-  const v8 = require('v8')
-  const heapStatsInverval = 10000;  // 10 seconds
+  // NOTE: set 'LOG_NODE_HEAPSTATS' env var to 'true' to log heap stats
+  // NOTE: set 'LOG_NODE_HEAPSTATS_INTERVAL_MS' env var to '<time_in_milliseconds>' for logging interval
+  const shouldLogHeapstats = config.heapstats.logHeapstats
+    ? config.heapstats.logHeapstats.toLowerCase() === 'true'
+    : false
 
-  // Function to add commas to a number
-  const addCommas = (num) => Number(num).toLocaleString();
+  if (shouldLogHeapstats) {
+    const v8 = require('v8')
 
-  // Function to log the heap space statistics
-  const logHeapSpaceStats = () => {
-    // Get the current timestamp
-    const currentTimestamp = new Date().toISOString();
-    
-    // Get the heap space statistics
-    const heapSpaceStats = v8.getHeapSpaceStatistics();
+    const addCommas = num => Number(num).toLocaleString()
+    const isNumeric = num => !isNaN(Number(num))
 
-    heapSpaceStats.forEach((space) => { 
-      const heapStatsMessage = `[${currentTimestamp}] Heap Space Statistics: `
-        + `Space Name: '${space.space_name}', `
-        + `Space Size: '${addCommas(space.space_size)}' bytes, `
-        + `Space Used Size: '${addCommas(space.space_used_size)}' bytes, `
-        + `Space Available Size: '${addCommas(space.space_available_size)}' bytes, `
-        + `Physical Space Size: '${addCommas(space.physical_space_size)}' bytes`
-        + '\n--------------------------';
+    // Set the heapstats logging interval
+    const maybeInterval = config.heapstats.logInverval
+    const heapStatsInverval = maybeInterval && isNumeric(maybeInterval) ? maybeInterval : 30000
 
-      logger.info(heapStatsMessage);
-    });
+    // Function to log the heap space statistics
+    const logHeapSpaceStats = () => {
+      // Get the current timestamp
+      const currentTimestamp = new Date().toISOString()
 
-    // Get the heap statistics
-    const heapStats = v8.getHeapStatistics();
+      // Get the heap space statistics
+      const heapSpaceStats = v8.getHeapSpaceStatistics()
 
-    const heapStatsMessage = `[${currentTimestamp}] Heap Statistics: `
-      + `Total Heap Size: '${addCommas(heapStats.total_heap_size)}' bytes, `
-      + `Total Heap Used Size: '${addCommas(heapStats.total_heap_size)}' bytes, `
-      + `Total Physical Size: '${addCommas(heapStats.total_physical_size)}' bytes, `
-      + `Total Available Size: '${addCommas(heapStats.total_available_size)}' bytes, `
-      + `Used Heap Size: '${addCommas(heapStats.used_heap_size)}' bytes, `
-      + `Heap Size Limit: '${addCommas(heapStats.heap_size_limit)}' bytes`
-      + '\n--------------------------';
+      heapSpaceStats.forEach(space => {
+        const heapStatsMessage =
+          `[${currentTimestamp}] Heap Space Statistics: ` +
+          `Space Name: '${space.space_name}', ` +
+          `Space Size: '${addCommas(space.space_size)}' bytes, ` +
+          `Space Used Size: '${addCommas(space.space_used_size)}' bytes, ` +
+          `Space Available Size: '${addCommas(space.space_available_size)}' bytes, ` +
+          `Physical Space Size: '${addCommas(space.physical_space_size)}' bytes` +
+          '\n--------------------------'
 
-      logger.info(heapStatsMessage);
-  };
+        logger.info(heapStatsMessage)
+      })
 
-  // Only run if not in a test environment
-  if (process.argv.every(arg => !arg.includes('mocha'))) {
-    // Set the interval to log the heap space statistics
-    setInterval(logHeapSpaceStats, heapStatsInverval);
+      // Get the heap statistics
+      const heapStats = v8.getHeapStatistics()
+
+      const heapStatsMessage =
+        `[${currentTimestamp}] Heap Statistics: ` +
+        `Total Heap Size: '${addCommas(heapStats.total_heap_size)}' bytes, ` +
+        `Total Heap Used Size: '${addCommas(heapStats.total_heap_size)}' bytes, ` +
+        `Total Physical Size: '${addCommas(heapStats.total_physical_size)}' bytes, ` +
+        `Total Available Size: '${addCommas(heapStats.total_available_size)}' bytes, ` +
+        `Used Heap Size: '${addCommas(heapStats.used_heap_size)}' bytes, ` +
+        `Heap Size Limit: '${addCommas(heapStats.heap_size_limit)}' bytes` +
+        '\n--------------------------'
+
+      logger.info(heapStatsMessage)
+    }
+
+    // Only run if not in a test environment
+    if (process.argv.every(arg => !arg.includes('mocha'))) {
+      // Set the interval to log the heap space statistics
+      setInterval(logHeapSpaceStats, heapStatsInverval)
+    }
   }
   // ===================================================
 
