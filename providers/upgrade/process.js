fa14a6d281787025f097e7d0a3813320bfe55a56
Qing Tomlinson <qing.tomlinson@sap.com>
2024-11-30 19:32:44 -0800

    Add integration tests

--- providers/upgrade/process.js
+++ providers/upgrade/process.js
@@ -59,7 +59,7 @@ class DefinitionUpgrader {
 let queueHandler
 let defUpgrader
 
-function setup(_queue, _definitionService, _logger, once = false, _defVersionChecker = factory()) {
+function setup(_queue, _definitionService, _logger, once = false, _defVersionChecker = factory({ logger: _logger })) {
   defUpgrader = new DefinitionUpgrader(_definitionService, _logger, _defVersionChecker)
   queueHandler = new QueueHandler(_queue, _logger, defUpgrader)
   return queueHandler.work(once)
