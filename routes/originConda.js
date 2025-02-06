afa490e13477e77268744d5b2387de95d6663f0e
yashkohli88 <yash.kohli@sap.com>
2025-02-06 21:59:09 +0530

    Refactored code to encode request parameters centrally

--- routes/originConda.js
+++ routes/originConda.js
@@ -10,11 +10,7 @@ router.get('/:channel/:subdir/:name/revisions', asyncMiddleware(getOriginCondaRe
 async function getOriginCondaRevisions(request, response) {
   let { channel, subdir, name } = request.params
   try {
-    const revisions = await repoAccess.getRevisions(
-      encodeURIComponent(channel),
-      encodeURIComponent(subdir),
-      encodeURIComponent(name)
-    )
+    const revisions = await repoAccess.getRevisions(channel, subdir, name)
     response.status(200).send(revisions)
   } catch (error) {
     response.status(404).send(error.message)
@@ -25,7 +21,7 @@ router.get('/:channel/:name', asyncMiddleware(getOriginConda))
 async function getOriginConda(request, response) {
   let { channel, name } = request.params
   try {
-    const matches = await repoAccess.getPackages(encodeURIComponent(channel), encodeURIComponent(name))
+    const matches = await repoAccess.getPackages(channel, name)
     response.status(200).send(matches)
   } catch (error) {
     response.status(404).send(error.message)
--- routes/originCondaforge.js
+++ routes/originCondaforge.js
@@ -13,7 +13,7 @@ router.get(
   asyncMiddleware(async (req, res) => {
     const { name, subdir } = req.params
     try {
-      const revisions = await repoAccess.getRevisions(channel, encodeURIComponent(subdir), encodeURIComponent(name))
+      const revisions = await repoAccess.getRevisions(channel, subdir, name)
       res.status(200).send(revisions)
     } catch (error) {
       res.status(404).send(error.message)
@@ -26,7 +26,7 @@ router.get(
   asyncMiddleware(async (req, res) => {
     const { name } = req.params
     try {
-      const matches = await repoAccess.getPackages(channel, encodeURIComponent(name))
+      const matches = await repoAccess.getPackages(channel, name)
       res.status(200).send(matches)
     } catch (error) {
       res.status(404).send(error.message)
