5d8b7da3186439fccc3bcb5125dd3f2870206825
yashkohli88 <yash.kohli@sap.com>
2025-02-06 17:02:11 +0530

    Refactor setup function for testing

--- routes/originConda.js
+++ routes/originConda.js
@@ -4,33 +4,40 @@
 const asyncMiddleware = require('../middleware/asyncMiddleware')
 const router = require('express').Router()
 
-function setup(repoAccess) {
-  router.get('/:channel/:subdir/:name/revisions', asyncMiddleware(getOriginCondaRevisions))
-  async function getOriginCondaRevisions(request, response) {
-    let { channel, subdir, name } = request.params
-    try {
-      const revisions = await repoAccess.getRevisions(
-        encodeURIComponent(channel),
-        encodeURIComponent(subdir),
-        encodeURIComponent(name)
-      )
-      response.status(200).send(revisions)
-    } catch (error) {
-      response.status(404).send(error.message)
-    }
-  }
+let repoAccess
 
-  router.get('/:channel/:name', asyncMiddleware(getOriginConda))
-
-  async function getOriginConda(request, response) {
-    let { channel, name } = request.params
-    try {
-      const matches = await repoAccess.getPackages(encodeURIComponent(channel), encodeURIComponent(name))
-      response.status(200).send(matches)
-    } catch (error) {
-      response.status(404).send(error.message)
-    }
+router.get('/:channel/:subdir/:name/revisions', asyncMiddleware(getOriginCondaRevisions))
+async function getOriginCondaRevisions(request, response) {
+  let { channel, subdir, name } = request.params
+  try {
+    const revisions = await repoAccess.getRevisions(
+      encodeURIComponent(channel),
+      encodeURIComponent(subdir),
+      encodeURIComponent(name)
+    )
+    response.status(200).send(revisions)
+  } catch (error) {
+    response.status(404).send(error.message)
   }
+}
+
+router.get('/:channel/:name', asyncMiddleware(getOriginConda))
+async function getOriginConda(request, response) {
+  let { channel, name } = request.params
+  try {
+    const matches = await repoAccess.getPackages(encodeURIComponent(channel), encodeURIComponent(name))
+    response.status(200).send(matches)
+  } catch (error) {
+    response.status(404).send(error.message)
+  }
+}
+
+function setup(condaRepoAccess, testflag = false) {
+  if (testflag) {
+    router._getOriginConda = getOriginConda
+    router._getOriginCondaRevisions = getOriginCondaRevisions
+  }
+  repoAccess = condaRepoAccess
   return router
 }
 module.exports = setup
--- routes/originCondaforge.js
+++ routes/originCondaforge.js
@@ -5,34 +5,37 @@ const express = require('express')
 const asyncMiddleware = require('../middleware/asyncMiddleware')
 const router = express.Router()
 
-function setup(repoAccess) {
-  const channel = 'conda-forge'
-  router.get(
-    '/:subdir/:name/revisions',
-    asyncMiddleware(async (req, res) => {
-      const { name, subdir } = req.params
-      try {
-        const revisions = await repoAccess.getRevisions(channel, encodeURIComponent(subdir), encodeURIComponent(name))
-        res.status(200).send(revisions)
-      } catch (error) {
-        res.status(404).send(error.message)
-      }
-    })
-  )
+const channel = 'conda-forge'
+let repoAccess
 
-  router.get(
-    '/:name',
-    asyncMiddleware(async (req, res) => {
-      const { name } = req.params
-      try {
-        const matches = await repoAccess.getPackages(channel, encodeURIComponent(name))
-        res.status(200).send(matches)
-      } catch (error) {
-        res.status(404).send(error.message)
-      }
-    })
-  )
+router.get(
+  '/:subdir/:name/revisions',
+  asyncMiddleware(async (req, res) => {
+    const { name, subdir } = req.params
+    try {
+      const revisions = await repoAccess.getRevisions(channel, encodeURIComponent(subdir), encodeURIComponent(name))
+      res.status(200).send(revisions)
+    } catch (error) {
+      res.status(404).send(error.message)
+    }
+  })
+)
 
+router.get(
+  '/:name',
+  asyncMiddleware(async (req, res) => {
+    const { name } = req.params
+    try {
+      const matches = await repoAccess.getPackages(channel, encodeURIComponent(name))
+      res.status(200).send(matches)
+    } catch (error) {
+      res.status(404).send(error.message)
+    }
+  })
+)
+
+function setup(condaForgeRepoAccess) {
+  repoAccess = condaForgeRepoAccess
   return router
 }
 
