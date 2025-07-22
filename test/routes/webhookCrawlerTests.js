c67c133a07c3986c82486dd9ed1593d1d22af0b0
ElaineDeMattosSilvaB <elaine.de-mattos-silva-bezerra@deutschebahn.com>
2025-07-22 12:03:23 +0200

    test: add extra check suggested by copilot
    Signed-off-by: ElaineDeMattosSilvaB <elaine.de-mattos-silva-bezerra@deutschebahn.com>

--- test/routes/webhookCrawlerTests.js
+++ test/routes/webhookCrawlerTests.js
@@ -22,6 +22,8 @@ describe('Webhook Route for Crawler calls', () => {
   it('handles missing self', () => {
     const request = createRequest()
     const response = httpMocks.createResponse()
+    expect(response.statusCode).to.be.eq(200)
+    expect(response._getData()).to.be.empty
     const logger = createLogger()
     const service = createDefinitionService()
     const router = webhookRoutes(null, service, logger, 'secret', 'secret', true)
