4eeb370e221c94ec9af9f2e269eff1b739d24287
justingosses_microsoft <11600445+JustinGOSSES@users.noreply.github.com>
2025-09-19 15:27:05 -0500

    take out no longer needed code

--- routes/notices.js
+++ routes/notices.js
@@ -8,7 +8,6 @@ const EntityCoordinates = require('../lib/entityCoordinates')
 const validator = require('../schemas/validator')
 const logger = require('../providers/logging/logger')
 
-router.post('/', asyncMiddleware(generateNotices))
 
 // Post a (set of) component to be included in a single NOTICE file
 router.post('/', bodyParser.json({ limit: '0.6mb' }), asyncMiddleware(generateNotices))
