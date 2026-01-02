b6cc6eb38098f9a7930c5650b052c1433d742404
ElaineDeMattosSilvaB <elaine.de-mattos-silva-bezerra@deutschebahn.com>
2026-01-02 13:27:38 +0100

    fix: add ajv type coercion for express 5

--- app.js
+++ app.js
@@ -163,8 +163,6 @@ function createApp(config) {
   app.post('/curations', batchApiLimiter)
   app.post('/notices', noticeApiLimiter)
 
-  app.use(require('./middleware/querystring'))
-
   app.use('/origins/github', require('./routes/originGitHub')())
   app.use('/origins/crate', require('./routes/originCrate')())
   const repoAccess = require('./lib/condaRepoAccess')()
