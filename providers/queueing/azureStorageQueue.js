898cbbe800352f1e35e680d4d3aee836bbeafe0d
Jamie Magee <jamie.magee@gmail.com>
2026-03-09 20:15:13 -0700

    Add type definitions for providers/queueing
    Define IQueue, QueueMessage, and DequeuedMessage interfaces in
    index.d.ts. Add .d.ts files for MemoryQueue and AzureStorageQueue.
    Add JSDoc annotations to the JS files so tsc can check them.
    Uses @ts-ignore for azure-storage SDK type mismatches, matching
    the pattern in providers/stores/abstractAzblobStore.js.

--- providers/queueing/azureStorageQueue.js
+++ providers/queueing/azureStorageQueue.js
@@ -5,7 +5,12 @@ const azure = require('azure-storage')
 const logger = require('../logging/logger')
 const { promisify } = require('util')
 
+/** @typedef {import('.').DequeuedMessage} DequeuedMessage */
+
 class AzureStorageQueue {
+  /**
+   * @param {import('./azureStorageQueue').AzureStorageQueueOptions} options
+   */
   constructor(options) {
     this.options = options
     this.logger = logger()
@@ -34,34 +39,45 @@ class AzureStorageQueue {
    * Returns null if the queue is empty
    * If DQ count exceeds 5 the message will be deleted and the next message will be returned
    *
-   * @returns {object} - { original: message, data: "JSON parsed, base64 decoded message" }
+   * @returns {Promise<DequeuedMessage | null>}
    */
   async dequeue() {
     const message = await promisify(this.queueService.getMessage).bind(this.queueService)(this.options.queueName)
     if (!message) return null
     if (message.dequeueCount <= 5)
+      // @ts-ignore - azure-storage QueueMessageResult is structurally compatible at runtime
       return { original: message, data: JSON.parse(Buffer.from(message.messageText, 'base64').toString('utf8')) }
+    // @ts-ignore - azure-storage QueueMessageResult used as QueueMessage
     await this.delete({ original: message })
     return this.dequeue()
   }
 
-  /** Similar to dequeue() but returns multiple messages to improve performance */
+  /**
+   * Similar to dequeue() but returns multiple messages to improve performance
+   *
+   * @returns {Promise<DequeuedMessage[]>}
+   */
   async dequeueMultiple() {
     const messages = await promisify(this.queueService.getMessages).bind(this.queueService)(
       this.options.queueName,
+      // @ts-ignore - azure-storage getMessages accepts options as second arg
       this.options.dequeueOptions
     )
     if (!messages || messages.length === 0) return []
     for (const i in messages) {
       if (messages[i].dequeueCount <= 5) {
+        // @ts-ignore - mutating array element from QueueMessageResult to DequeuedMessage
         messages[i] = {
+          // @ts-ignore
           original: messages[i],
           data: JSON.parse(Buffer.from(messages[i].messageText, 'base64').toString('utf8'))
         }
       } else {
+        // @ts-ignore - azure-storage QueueMessageResult used as QueueMessage
         await this.delete({ original: messages[i] })
       }
     }
+    // @ts-ignore - array has been mutated to DequeuedMessage[]
     return messages
   }
 
@@ -69,12 +85,12 @@ class AzureStorageQueue {
    * Delete a recently DQ'd message from the queue
    * pass dequeue().original as the message to delete
    *
-   * @param {object} message
+   * @param {DequeuedMessage} message
    */
   async delete(message) {
     await promisify(this.queueService.deleteMessage).bind(this.queueService)(
       this.options.queueName,
-      message.original.messageId,
+      String(message.original.messageId),
       message.original.popReceipt
     )
   }
